be1d32d28885a32d9b051f2582c90e7ddce859dd
Mallignos <mattias.aronssonadler.9978@student.uu.se>
2021-06-24 16:18:12 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,88 @@
+from plcom import SicstusCommunicator
+from typing import Union,Literal
+import json
+
+Sudoku_char_t = Union[int,Literal['_']]
+Sudoku_t = list[list[Sudoku_char_t]]
+
+def pretty_print_sudoku(board : Sudoku_t) -> None:
+    header    = "┏━━━━━━━┯━━━━━━━┯━━━━━━━┓"
+    breakline = "┠───────┼───────┼───────┨"
+    footer    = "┗━━━━━━━┷━━━━━━━┷━━━━━━━┛"
+
+    print(header)
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[0])))
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[1])))
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[2])))
+    print(breakline)
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[3])))
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[4])))
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[5])))
+    print(breakline)
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[6])))
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[7])))
+    print("┃ %s %s %s │ %s %s %s │ %s %s %s ┃" % tuple(map(str,board[8])))
+    print(footer)
+
+
+sudoku_board_1 : Sudoku_t = [
+    ['_',6,'_','_',1,5,7,'_',8],
+    [5,'_','_',7,'_',3,2,4,'_'],
+    ['_',7,3,'_','_','_',6,'_','_'],
+
+    ['_',9,'_',1,5,7,3,8,'_'],
+    ['_','_',2,'_',9,6,'_','_','_'],
+    ['_','_','_',2,8,'_','_',1,'_'],
+
+    [1,3,'_','_','_',2,'_','_',4],
+    ['_','_','_','_','_',1,5,2,7],
+    [7,2,8,'_','_','_','_',6,'_']
+]
+
+sudoku_board_2 : Sudoku_t = [
+    ['_','_','_','_',5,'_','_','_',9],
+    [4,'_','_','_','_',6,'_','_',1],
+    ['_','_',1,'_','_',3,'_',5,'_'],
+
+    ['_','_','_','_','_',8,4,'_','_'],
+    ['_','_',7,'_','_','_','_','_','_'],
+    ['_',2,'_',1,9,'_','_',8,'_'],
+
+    ['_','_',9,'_','_','_','_',3,'_'],
+    [6,'_','_','_',3,4,'_','_','_'],
+    [3,'_','_','_','_','_',7,'_','_']
+]
+
+def stringify_board(board : Sudoku_t) -> str:
+    out : list[str] = []
+    for row in board:
+        out.append('[' + ','.join(map(str,row)) + ']')
+    return '[' + ','.join(out) + ']'
+
+print_debug=False
+sc = SicstusCommunicator(consultFile="sudoku",debug=print_debug)
+
+#sc.once("consult(sudoku).")
+sc.once("use_module(library(codesio)).")
+sc.once("assert((decode_string(Input,O) :- name(Input,A),append(A,\".\",S),read_from_codes(S,O))).")
+
+sc.state( stringify_board(sudoku_board_1) )
+
+#print(sc.state())
+
+stop = False
+#for board in sc.call("P=" + stringify_board(sudoku_board_1) + ",sudoku(P,3),Result=P."):
+for board in sc.call("decode_string(StateIn,P),sudoku(P,3),Result=P."):
+
+    pretty_print_sudoku(board)
+    
+    while True:
+        inp = str(input(" ? fler svar? (ja/NEJ) "))
+        if inp.lower() in [';','ja','j']:
+            break
+        elif inp.lower() in ['','n','nej']:
+            stop = True
+            break
+    if stop is True:
+        break
+
---
+++ plcom.py
@@ -0,0 +1,129 @@
+#import os
+#import sys
+import json
+import subprocess
+from typing import Optional,Any,Union
+
+
+class SicstusCommunicator:
+    #proc : subprocess.Popen[str]
+
+    _idcounter : int = 0
+
+    def __init__(self,
+        consultFile : Union[None,str,list[str]] = None,
+        exe_path : str = 'sicstus',
+        pl_path : str = '$SP_LIBRARY_DIR/jsonrpc/jsonrpc_server',
+        debug : bool = False
+    ):
+        self.debug = debug
+        procArgs = [ exe_path, '-l', pl_path ]
+
+        if type(consultFile) is list[str]:
+            for f in consultFile:
+                procArgs += ['-l',f]
+        elif type(consultFile) is str:
+            procArgs += ['-l', consultFile ]
+        elif consultFile is None:
+            pass
+        else:
+            pass
+
+        procArgs += [ '--goal', 'jsonrpc_server_main([call_hook(call)]),halt.' ]
+
+        self.proc = subprocess.Popen(
+            procArgs,
+            stdout=subprocess.PIPE,
+            stderr=None if debug else subprocess.PIPE, #Hacky maybe?
+            stdin=subprocess.PIPE,
+            encoding='UTF-8'
+        )
+
+    def __del__(self):
+        request_quit = json.dumps({'jsonrpc':'2.0','id':self.idcounter ,'method':'quit'})
+        self._write_to_proc(request_quit)
+        
+        m = self._read_from_proc()
+        if self.debug:
+            print(m)
+
+    @property
+    def idcounter(self) -> int:
+        self._idcounter += 1
+        return self._idcounter - 1
+
+    @idcounter.setter
+    def idcounter(self,value : int):
+        self._idcounter = value
+
+    def _write_to_proc(self,msg : str) -> None:
+        if self.proc.stdin is not None:
+            self.proc.stdin.write(msg)
+            self.proc.stdin.flush()
+        else:
+            pass
+            # Probably raise some Error? We should never have this issue
+
+    def _read_from_proc(self) -> dict[Any,Any]:
+        if self.proc.stdout is not None:
+            v = self.proc.stdout.readline()
+            return json.loads(v)
+        else:
+            # Maybe raise some error instead?
+            return dict()
+
+    def once(self,goal : str) -> Any:
+        msg = json.dumps({'jsonrpc':'2.0','id':self.idcounter ,'method':'once','params':{'goal':goal}})
+        self._write_to_proc(msg)
+        d = self._read_from_proc()
+        if self.debug:
+            print(d)
+        return d['result']
+    
+    def state(self, goal : Optional[Any] = None) -> Any:
+        if goal is None:
+            msg = json.dumps({'jsonrpc':'2.0','id':self.idcounter ,'method':'state'})
+        else:
+            msg = json.dumps({'jsonrpc':'2.0','id':self.idcounter ,'method':'state','params':[goal]})
+        self._write_to_proc(msg)
+        d = self._read_from_proc()
+        if self.debug:
+            print(d)
+        return d['result']
+    
+    def call(self,goal : str) -> Any:
+        finished = False
+        try:
+            msg = json.dumps({'jsonrpc':'2.0','id':self.idcounter ,'method':'call','params':{'goal':goal}})
+            self._write_to_proc(msg)
+
+            d = self._read_from_proc()
+            while True:
+                if self.debug:
+                    print(d)
+                if 'error' in d:
+                    if d['error']['code'] == -4711:
+                        #self._cut()
+                        finished = True
+                        return
+                    raise RuntimeError # TODO: Specialize me
+                yield d.get("result",None)
+                d = self._retry()
+        finally:
+            if finished is False:
+                self._cut()
+
+    def _cut(self) -> None:
+        msg = json.dumps({'jsonrpc':'2.0','id':self.idcounter ,'method':'cut'})
+        self._write_to_proc(msg)
+        d = self._read_from_proc()
+        if self.debug:
+            print(d)
+    
+    def _retry(self) -> dict[Any,Any]:
+        msg = msg = json.dumps({'jsonrpc':'2.0','id':self.idcounter ,'method':'retry'})
+        self._write_to_proc(msg)
+        d = self._read_from_proc()
+        return d
+
+
